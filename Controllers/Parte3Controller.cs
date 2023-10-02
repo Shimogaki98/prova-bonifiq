ffcaccdebc21a3334385686c6bf32557ef8c4cb8
MarcelloBronzatti <mbsimogaki@gmail.com>
2023-10-02 14:18:55 -0300

    factory criada para evitar estruturas IF, utilizando interfaces

--- Controllers/Parte3Controller.cs
+++ Controllers/Parte3Controller.cs
@@ -1,25 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProvaPub.Models;
-using ProvaPub.Repository;
 using ProvaPub.Services;
 
 namespace ProvaPub.Controllers
 {
-	
-	/// <summary>
-	/// Esse teste simula um pagamento de uma compra.
-	/// O método PayOrder aceita diversas formas de pagamento. Dentro desse método é feita uma estrutura de diversos "if" para cada um deles.
-	/// Sabemos, no entanto, que esse formato não é adequado, em especial para futuras inclusões de formas de pagamento.
-	/// Como você reestruturaria o método PayOrder para que ele ficasse mais aderente com as boas práticas de arquitetura de sistemas?
-	/// </summary>
-	[ApiController]
-	[Route("[controller]")]
-	public class Parte3Controller :  ControllerBase
-	{
-		[HttpGet("orders")]
-		public async Task<Order> PlaceOrder(string paymentMethod, decimal paymentValue, int customerId)
-		{
-			return await new OrderService().PayOrder(paymentMethod, paymentValue, customerId);
-		}
-	}
+
+    /// <summary>
+    /// Esse teste simula um pagamento de uma compra.
+    /// O método PayOrder aceita diversas formas de pagamento. Dentro desse método é feita uma estrutura de diversos "if" para cada um deles.
+    /// Sabemos, no entanto, que esse formato não é adequado, em especial para futuras inclusões de formas de pagamento.
+    /// Como você reestruturaria o método PayOrder para que ele ficasse mais aderente com as boas práticas de arquitetura de sistemas?
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class Parte3Controller : ControllerBase
+    {
+        private readonly OrderService _orderService;
+        private readonly IPaymentService _paymentService;
+
+        public Parte3Controller()
+        {
+            _orderService = new OrderService();
+            _paymentService = new PaymentService();
+        }
+
+        [HttpPost("orders")]
+        public async Task<Order> PlaceOrder(string paymentMethod, decimal paymentValue, int customerId)
+        {
+
+            var paymentClass = _paymentService.CreatePaymentClass(paymentMethod);
+            return await _orderService.PayOrder(paymentClass, paymentValue, customerId);
+        }
+    }
 }
--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -2,27 +2,79 @@
 
 namespace ProvaPub.Services
 {
-	public class OrderService
-	{
-		public async Task<Order> PayOrder(string paymentMethod, decimal paymentValue, int customerId)
-		{
-			if (paymentMethod == "pix")
-			{
-				//Faz pagamento...
-			}
-			else if (paymentMethod == "creditcard")
-			{
-				//Faz pagamento...
-			}
-			else if (paymentMethod == "paypal")
-			{
-				//Faz pagamento...
-			}
+    public class OrderService
+    {
+        public async Task<Order> PayOrder(IPaymentMethod paymentMethod, decimal paymentValue, int customerId)
+        {
+            paymentMethod.Pay(paymentValue, customerId);
 
-			return await Task.FromResult( new Order()
-			{
-				Value = paymentValue
-			});
-		}
-	}
+            return await Task.FromResult(new Order()
+            {
+                Id = customerId,
+                Value = paymentValue,
+            });
+        }
+    }
+
+    // Abstração para possível extensão de metodos de pagamento, sem alterar o método "Original" respeitando o princípio Open-Closed 
+
+    public interface IPaymentMethod
+    {
+        public void Pay(decimal paymentValue, int customerId);
+    }
+
+    public class Pix : IPaymentMethod
+    {
+        public void Pay(decimal paymentValue, int customerId)
+        {
+            // Pay with Pix
+        }
+    }
+    public class CreditCard : IPaymentMethod
+    {
+        public void Pay(decimal paymentValue, int customerId)
+        {
+            // Pay with Credit Card
+        }
+    }
+
+    public class Paypal : IPaymentMethod
+    {
+        public void Pay(decimal paymentValue, int customerId)
+        {
+            // Pay with Paypal
+        }
+    }
+
+
+    // Service e factory para evitar IF nos Controllers e Services
+    public interface IPaymentService
+    {
+        IPaymentMethod CreatePaymentClass(string payment);
+    }
+
+    public class PaymentService : IPaymentService
+    {
+        private readonly Dictionary<string, Func<IPaymentMethod>> _PaymentMethodFactories;
+
+        public PaymentService()
+        {
+            _PaymentMethodFactories = new Dictionary<string, Func<IPaymentMethod>>
+        {
+            { "pix".ToLower(), () => new Pix() },
+            { "creditcard".ToLower(), () => new CreditCard() },
+            { "paypal".ToLower(), () => new Paypal() },
+        };
+        }
+
+        public IPaymentMethod CreatePaymentClass(string payment)
+        {
+            if (_PaymentMethodFactories.TryGetValue(payment, out var factory))
+            {
+                return factory();
+            }
+
+            throw new ArgumentException("Invalid Payment Method");
+        }
+    }
 }
