4f2c2610cb36d6e9552a29240c39265b15d7409f
MarcelloBronzatti <mbsimogaki@gmail.com>
2023-10-02 11:51:32 -0300

    Parte 2 - Paginacao e Injecao de dependencias ok

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -1,21 +1,25 @@
-﻿using ProvaPub.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using ProvaPub.Models;
 using ProvaPub.Repository;
 
 namespace ProvaPub.Services
 {
-	public class ProductService
-	{
-		TestDbContext _ctx;
+    public class ProductService
+    {
+        TestDbContext _ctx;
 
-		public ProductService(TestDbContext ctx)
-		{
-			_ctx = ctx;
-		}
+        public ProductService(TestDbContext ctx)
+        {
+            _ctx = ctx;
+        }
 
-		public ProductList  ListProducts(int page)
-		{
-			return new ProductList() {  HasNext=false, TotalCount =10, Products = _ctx.Products.ToList() };
-		}
+        public async Task<List<Product>> ListProducts(int page)
+        {
+            //return new ProductList() { HasNext = false, TotalCount = 10, Products = _ctx.Products.ToList() };
+            int pageSize = 10;
 
-	}
+            return await _ctx.Products.OrderBy(p => p.Id).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+
+    }
 }
