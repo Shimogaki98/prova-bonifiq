21dd2a3016aae141ad95add1a8eca70a2b14b425
MarcelloBronzatti <mbsimogaki@gmail.com>
2023-10-02 11:25:25 -0300

    Parte 1 - possível solucao, removendo seed, gerando aleatorios em um curto periodo de tempo pode nao alterar o seed, gerando o mesmo numero.

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -1,16 +1,17 @@
 ﻿namespace ProvaPub.Services
 {
-	public class RandomService
-	{
-		int seed;
-		public RandomService()
-		{
-			seed = Guid.NewGuid().GetHashCode();
-		}
-		public int GetRandom()
-		{
-			return new Random(seed).Next(100);
-		}
+    public class RandomService
+    {
+        // int seed;
+        Random random = new Random();
+        public RandomService()
+        {
+            // seed = Guid.NewGuid().GetHashCode();
+        }
+        public int GetRandom()
+        {
+            return random.Next(0, 100);
+        }
 
-	}
+    }
 }
